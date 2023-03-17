39d174740e054cb4e38e2ab07c054611dd8be935
MrsLRikhotso <segomatladi@gmail.com>
2023-03-17 02:42:42 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - this is the main function
+*
+* Description: the program is driving me crazy
+*
+* Return : always 0 - Success
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
