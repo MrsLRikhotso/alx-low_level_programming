7132b466ee8c8bb209837826048b79470a7a5824
MrsLRikhotso <segomatladi@gmail.com>
2023-03-17 03:39:13 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -this the main function
+ *
+ * Return: 0 when succesful
+ */
+
+int main(void)
+{
+	printf("ith proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
