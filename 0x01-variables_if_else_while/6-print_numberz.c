0b09ab496546a727a41f609cbbc3db99cd41e643
MrsLRikhotso <segomatladi@gmail.com>
2023-03-19 02:42:06 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,11 +8,8 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 9; n++)
-	{
-		putchar("%d", n);
-
-	}
-	printf("\n");
+	for (n = 0 ; n < 10 ; n++)
+		putchar(n + '0');
+	putchar('\n');
 	return (0);
 }
