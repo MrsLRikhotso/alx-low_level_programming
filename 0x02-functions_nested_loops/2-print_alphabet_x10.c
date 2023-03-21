49900a902dea041d058df52c3c4d8f5c6f76bf73
MrsLRikhotso <segomatladi@gmail.com>
2023-03-21 03:21:13 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,16 @@
 
 void print_alphabet_x10(void)
 {
-	char c;
-	int i = 0;
+	int i;
+	int j;
 
-	while  (i <= 0)
+	for (j = 1; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
 
-	for  (c = 'a'; c <= 'z'; c++)
-	_putchar(c);
-
+		}
 	_putchar('\n');
-	i++;
+	}
 }
