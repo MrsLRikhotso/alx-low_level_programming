7a946a24f694a6609fbc89b0234d606b61a1568f
MrsLRikhotso <segomatladi@gmail.com>
2023-03-27 02:40:46 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
@@ -8,9 +7,9 @@
  */
 int main(void)
 {
-        char *str;
+    char *str;
 
-        str = "I do not fear computers, I fear the lack of them. - Isaac Asimov";
-        _puts(str);
-        return (0);
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    print_rev(str);
+    return (0);
 }
